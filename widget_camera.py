e05c6e0fd8a1d17bcdfcb6c59ca00a6acfb27663
razhterize <arza.mahendra.31@gmail.com>
2022-06-07 12:06:18 +0700

    updating image capture

--- widget_camera.py
+++ widget_camera.py
@@ -137,11 +137,8 @@ class WidgetCamera(QWidget):
             msg.exec()
 
     def image_capture(self):
-        ret, frame = self.cap.read()
-        if not ret:
-            YOLOGGER.info("Failed to capture image!")
         img_name = f"{self.path}/capture_{self.x}.png"
-        cv2.imwrite(img_name, frame)
+        cv2.imwrite(img_name, self.image)
         YOLOGGER.info("Image captured")
         self.x += 1
 
@@ -194,11 +191,10 @@ class WidgetCamera(QWidget):
         qp.drawRect(rect)
 
         sw, sh = self.width(), self.height()  # 图像窗口宽高
-        print(sh)
-        print(sw)
+    
 
-        if not self.opened:
-            QPainter.drawPixmap(self.height(), self.width(), 200, 200, QPixmap('img/video.svg'))
+        # if not self.opened:
+        #     qp.drawPixmap((sw-1)/2-100, sh/2-100, 200, 200, QPixmap('img/video.svg'))
 
         # 画图
         if self.opened and self.image is not None:
